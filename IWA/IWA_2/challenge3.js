b9adc3926f24ea289dfd9b5710680bb98bda9f98
lubabalo006 <137266437+lubabalo006@users.noreply.github.com>
2023-08-02 11:53:55 +0200

    updated challenge 1,2,3

--- IWA/IWA_2/challenge3.js
+++ IWA/IWA_2/challenge3.js
@@ -5,8 +5,8 @@ It is important to show the following message in order to:
 - to impress investors
 */
 
-console.warn('Security scan starting')
+console.warn('Security scan starting');
 
 /* It is important to let user know when they can close the page  */
 
-console.info('Please wait for scan to complete before closing the browser.')
+console.info('Please wait for scan to complete before closing the browser.'); 
