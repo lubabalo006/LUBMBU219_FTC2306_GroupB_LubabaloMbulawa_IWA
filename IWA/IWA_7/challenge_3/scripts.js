979ffcb192bf31ef76a897381b79e38860b5c4fd
lubabalo006 <137266437+lubabalo006@users.noreply.github.com>
2023-08-17 10:31:24 +0200

    IWA9, challenge1

--- IWA/IWA_7/challenge_3/scripts.js
+++ IWA/IWA_7/challenge_3/scripts.js
@@ -13,18 +13,18 @@ const divider = '----------------------------------'
 const currency='R'
 const owed = Math.abs(parseFloat(leoBalance)) + Math.abs(parseFloat(sarahBalance));
 
-var owedAmount = owed.toFixed(2).toString();
+let owedAmount = owed.toFixed(2);
+const [integerAmount, decimalAmount] = owedAmount.split(".")
+const spacedAmount = integerAmount.replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+const totalAmount = `${spacedAmount}.${decimalAmount}`
 
-if(owedAmount.length > 3){
-    const thousandSeparater = -6;
-    const strgOwedAmount = owedAmount.slice(0, owedAmount.length+thousandSeparater);
-    const strgspace = (" " + owedAmount.slice(thousandSeparater));
-    owedAmount = strgOwedAmount + strgspace;
-}
+
 
 const leo = `${leoName} ${leoSurname.trim()} (Owed: ${currency} ${Math.abs(parseFloat(sarahBalance)).toFixed(2)})`
 const sarah = `${sarahName.trim()} ${sarahSurname} (Owed: ${currency} ${Math.abs(parseFloat(leoBalance).toFixed(2)).toFixed(2)})`
-const total = `Total amount owed: ${currency} ${owedAmount}`;
-const result = ` ${leo} \n ${sarah} \n \n ${divider} \n   ${total} \n ${divider} \n `;
+const total = `Total amount owed: ${currency} ${totalAmount}`;
+const result = ` ${leo} \n ${sarah} \n \n ${divider} \n\t${total} \n ${divider} \n `;
 
 console.log(result);
+
+
